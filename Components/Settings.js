7bf0cfa29a5137c708204bf2a8a3e0620c5c0e62
Aile <600eli@gmail.com>
2019-10-27 20:59:21 +0100

    Fix other version contrast and adjust dimension of setting icon.

--- Components/Settings.js
+++ Components/Settings.js
@@ -1,27 +1,35 @@
 import React from "react";
-import { StyleSheet, Picker, Image, View, TouchableNativeFeedback} from "react-native";
+import {
+  StyleSheet,
+  Picker,
+  Image,
+  View,
+  TouchableNativeFeedback
+} from "react-native";
 
 export default class Settings extends React.Component {
-
-  onPress = () => {
-    
-  }
+  onPress = () => {};
 
   render() {
     return (
       <View style={styles.container}>
         <TouchableNativeFeedback onPress={this.onPress}>
-            <Image source={require('../assets/settings.png')}
-                   width={26} />
+          <Image
+            style={styles.image}
+            source={require("../assets/settings.png")}
+          />
         </TouchableNativeFeedback>
       </View>
-
-)
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 10,
+    paddingHorizontal: 10
+  },
+  image: {
+    width: 60,
+    height: 60
   }
-})
+});
